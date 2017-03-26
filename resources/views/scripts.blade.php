3758887e057a3ff634b10b03d864f9c44f2c5e44
Angel Flores <aaflores93@gmail.com>
2017-03-26 15:38:58 -0600

    piano layoit

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -5,7 +5,8 @@
 
     <title>Laravel</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
+    <!-- Piano Layout -->
+    <link rel="stylesheet" href="{{asset('css\pianoLayout.css')}}">
     <!-- Fonts -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
