8361e96ae1a8b13c63c54f2a966fe6a306717e46
Angel Flores <aaflores93@gmail.com>
2017-03-26 23:02:57 -0600

    styling interface

--- resources/views/Generated/generatePop.blade.php
+++ resources/views/Generated/generatePop.blade.php
@@ -10,123 +10,112 @@
     <div id="pop" class="panel-collapse collapse">
         <div class="panel-body">
             <div class="container-fluid">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4><strong>Verse options</strong></h4>
-                                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4><strong>Verse options</strong></h4>
                             </div>
-                            <div class="row">
-                                <div class="form-group">
-                                    <div class="btn-group">
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="btn-group">
 
-                                        @foreach($popProgressions['verse'] as $progression)
-                                        <div class="col-md-12">
-                                            <div class="row">
-                                                <div class="col-md-1">
-                                                    {{ Form::radio('verse',$progression['progression']) }}
-                                                </div>
-                                                @foreach($progression['chords'] as $chord)
-                                                 <div class="col-md-2">
-                                                     {{$chord}}
-                                                 </div>
-                                                @endforeach
-                                            </div>
+                                    @foreach($popProgressions['verse'] as $progression)
+                                    <div class="col-md-12">
+                                        <div class="row">
+
+                                            @foreach($progression['chords'] as $chord)
+                                             <div class="col-md-2">
+                                                 {{$chord}}
+                                             </div>
+                                            @endforeach
                                         </div>
-                                        @endforeach
                                     </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4"></div>
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4><strong>Chorus options</strong></h4>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="form-group">
-                                    <div class="btn-group">
-                                        @foreach($popProgressions['chorus'] as $progression)
-                                        <div class="col-md-12">
-                                            <div class="row">
-                                                <div class="col-md-1">
-                                                    {{ Form::radio('chorus',$progression['progression']) }}
-                                                </div>
-                                                @foreach($progression['chords'] as $chord)
-                                                <div class="col-md-2">
-                                                    {{$chord}}
-                                                </div>
-                                                @endforeach
-                                            </div>
-                                        </div>
-                                        @endforeach
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4"></div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4><strong>Prechorus options</strong></h4>
-                                </div>
+
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4><strong>Chorus options</strong></h4>
                             </div>
-                            <div class="row">
-                                <div class="form-group">
-                                    <div class="btn-group">
-                                        @foreach($popProgressions['prechorus'] as $progression)
-                                        <div class="col-md-12">
-                                            <div class="row">
-                                                <div class="col-md-1">
-                                                    {{ Form::radio('prechorus',$progression['progression']) }}
-                                                </div>
-                                                @foreach($progression['chords'] as $chord)
-                                                <div class="col-md-2">
-                                                    {{$chord}}
-                                                </div>
-                                                @endforeach
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="btn-group">
+                                    @foreach($popProgressions['chorus'] as $progression)
+                                    <div class="col-md-12">
+                                        <div class="row">
+
+                                            @foreach($progression['chords'] as $chord)
+                                            <div class="col-md-2">
+                                                {{$chord}}
                                             </div>
+                                            @endforeach
                                         </div>
-                                        @endforeach
                                     </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4"></div>
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4><strong>Bridge options</strong></h4>
-                                </div>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4><strong>Prechorus options</strong></h4>
                             </div>
-                            <div class="row">
-                                <div class="form-group">
-                                    <div class="btn-group">
-                                        @foreach($popProgressions['bridge'] as $progression)
-                                        <div class="col-md-12">
-                                            <div class="row">
-                                                <div class="col-md-1">
-                                                    {{ Form::radio('bridge',$progression['progression']) }}
-                                                </div>
-                                                @foreach($progression['chords'] as $chord)
-                                                <div class="col-md-2">
-                                                    {{$chord}}
-                                                </div>
-                                                @endforeach
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="btn-group">
+                                    @foreach($popProgressions['prechorus'] as $progression)
+                                    <div class="col-md-12">
+                                        <div class="row">
+
+                                            @foreach($progression['chords'] as $chord)
+                                            <div class="col-md-2">
+                                                {{$chord}}
                                             </div>
+                                            @endforeach
                                         </div>
-                                        @endforeach
                                     </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4"></div>
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4><strong>Bridge options</strong></h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="btn-group">
+                                    @foreach($popProgressions['bridge'] as $progression)
+                                    <div class="col-md-12">
+                                        <div class="row">
+
+                                            @foreach($progression['chords'] as $chord)
+                                            <div class="col-md-2">
+                                                {{$chord}}
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/Generated/generateRock.blade.php
+++ resources/views/Generated/generateRock.blade.php
@@ -10,123 +10,110 @@
     <div id="rock" class="panel-collapse collapse">
         <div class="panel-body">
             <div class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4><strong>Verse options</strong></h4>
-                                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4><strong>Verse options</strong></h4>
                             </div>
-                            <div class="row">
-                                <div class="form-group">
-                                    <div class="btn-group">
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="btn-group">
 
-                                        @foreach($rockProgressions['verse'] as $progression)
-                                            <div class="col-md-12">
-                                                <div class="row">
-                                                    <div class="col-md-1">
-                                                        {{ Form::radio('verse',$progression['progression']) }}
+                                    @foreach($rockProgressions['verse'] as $progression)
+                                        <div class="col-md-12">
+                                            <div class="row">
+
+                                                @foreach($progression['chords'] as $chord)
+                                                    <div class="col-md-2">
+                                                        {{$chord}}
                                                     </div>
-                                                    @foreach($progression['chords'] as $chord)
-                                                        <div class="col-md-2">
-                                                            {{$chord}}
-                                                        </div>
-                                                    @endforeach
-                                                </div>
+                                                @endforeach
                                             </div>
-                                        @endforeach
-                                    </div>
+                                        </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4"></div>
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4><strong>Chorus options</strong></h4>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="form-group">
-                                    <div class="btn-group">
-                                        @foreach($rockProgressions['chorus'] as $progression)
-                                            <div class="col-md-12">
-                                                <div class="row">
-                                                    <div class="col-md-1">
-                                                        {{ Form::radio('chorus',$progression['progression']) }}
-                                                    </div>
-                                                    @foreach($progression['chords'] as $chord)
-                                                        <div class="col-md-2">
-                                                            {{$chord}}
-                                                        </div>
-                                                    @endforeach
-                                                </div>
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4"></div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4><strong>Prechorus options</strong></h4>
-                                </div>
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4><strong>Chorus options</strong></h4>
                             </div>
-                            <div class="row">
-                                <div class="form-group">
-                                    <div class="btn-group">
-                                        @foreach($rockProgressions['prechorus'] as $progression)
-                                            <div class="col-md-12">
-                                                <div class="row">
-                                                    <div class="col-md-1">
-                                                        {{ Form::radio('prechorus',$progression['progression']) }}
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="btn-group">
+                                    @foreach($rockProgressions['chorus'] as $progression)
+                                        <div class="col-md-12">
+                                            <div class="row">
+
+                                                @foreach($progression['chords'] as $chord)
+                                                    <div class="col-md-2">
+                                                        {{$chord}}
                                                     </div>
-                                                    @foreach($progression['chords'] as $chord)
-                                                        <div class="col-md-2">
-                                                            {{$chord}}
-                                                        </div>
-                                                    @endforeach
-                                                </div>
+                                                @endforeach
                                             </div>
-                                        @endforeach
-                                    </div>
+                                        </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4"></div>
-                        <div class="col-md-8">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h4><strong>Bridge options</strong></h4>
-                                </div>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4><strong>Prechorus options</strong></h4>
                             </div>
-                            <div class="row">
-                                <div class="form-group">
-                                    <div class="btn-group">
-                                        @foreach($rockProgressions['bridge'] as $progression)
-                                            <div class="col-md-12">
-                                                <div class="row">
-                                                    <div class="col-md-1">
-                                                        {{ Form::radio('bridge',$progression['progression']) }}
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="btn-group">
+                                    @foreach($rockProgressions['prechorus'] as $progression)
+                                        <div class="col-md-12">
+                                            <div class="row">
+
+                                                @foreach($progression['chords'] as $chord)
+                                                    <div class="col-md-2">
+                                                        {{$chord}}
                                                     </div>
-                                                    @foreach($progression['chords'] as $chord)
-                                                        <div class="col-md-2">
-                                                            {{$chord}}
-                                                        </div>
-                                                    @endforeach
-                                                </div>
+                                                @endforeach
                                             </div>
-                                        @endforeach
-                                    </div>
+                                        </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4"></div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4><strong>Bridge options</strong></h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group">
+                                <div class="btn-group">
+                                    @foreach($rockProgressions['bridge'] as $progression)
+                                        <div class="col-md-12">
+                                            <div class="row">
+
+                                                @foreach($progression['chords'] as $chord)
+                                                    <div class="col-md-2">
+                                                        {{$chord}}
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
